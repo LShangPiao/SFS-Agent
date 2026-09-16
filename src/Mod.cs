d85e26764373e9e1aacd48f452160e8ecc0cbe68
LShangPiao <1565873375@qq.com>
2026-09-17 01:47:50 +0800

    版本号统一为 v0.3.0

--- src/Mod.cs
+++ src/Mod.cs
@@ -55,7 +55,7 @@ namespace SfsAgent
 
         public override string ModVersion
         {
-            get { return "v0.1.0"; }
+            get { return "v0.3.0"; }
         }
 
         public override string Description
